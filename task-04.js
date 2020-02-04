382af0799607e554a8bfd9ab23123f81e5727c28
Igor128157 <igor0063@ukr.net>
2020-02-04 10:08:43 +0200

    add next

--- task-04.js
+++ task-04.js
@@ -4,20 +4,19 @@ const credits = 23580;
 
 const pricePerDroid = 3000;
 
-const buyDroids = prompt ('Какое количество дроидов Вы хотите купить?');
+let buyDroids = prompt ('Какое количество дроидов Вы хотите купить?');
 
 let totalPrice;
-let balanceCredits;
 
 if (buyDroids === null) {
   console.log('Отменено пользователем!');
 } else   {
   totalPrice = pricePerDroid * buyDroids;
-}
 
 if (totalPrice > credits) {
   console.log('Недостаточно средств на счету!');
-} else if (totalPrice < credits) {
-  balanceCredits = credits - (pricePerDroid * buyDroids);
-  console.log(`Вы купили ${buyDroids} дроидов, на счету осталось ${balanceCredits} кредитов`);
+} else  {
+  credits - totalPrice;
+  console.log(`Вы купили ${buyDroids} дроидов, на счету осталось ${credits - totalPrice} кредитов`);
+}
 }
